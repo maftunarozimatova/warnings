47c9b86180e3c1a1c6299926b29f09e4d204d96e
Maftuna <156097566+maftunarozimatova@users.noreply.github.com>
2024-03-04 17:42:08 +0500

    warnings

---
+++ main.js
@@ -0,0 +1,56 @@
+// // M - 1
+
+let Anna = prompt("1-number")
+let Bella = prompt("2-number")
+console.log(Anna + Bella);
+
+// // M - 2
+
+let Jek = -prompt("First Number")
+let Jekki = -prompt("Secont Name")
+console.log(Jekki - Jekki);
+
+// // M - 3
+
+let con = confirm("Keyingi misolga o'tishni hohlaysizmi ?")
+
+// // M - 4
+
+let Merri = +prompt("1-son")
+let Merci = +prompt("2-son")
+console.log(Merri + Merci);
+
+// // M - 5
+
+let h = -prompt("1-son")
+let x = -prompt("2-son")
+console.log(h -x);
+
+// M - 6
+let o = confirm("Davom ettirasizmi ?")
+
+// M - 7
+
+let a = prompt("1-raqam ")
+let b =  prompt("2-raqam ")
+let k = (a + b )
+
+// M - 8
+
+let c = -prompt("1-raqam")
+let d = -prompt("2-raqam")
+let t = (c - d)
+
+// M - 9
+
+let B = alert("Keyingi misollarga o'ting")
+
+// M - 10
+
+let q = prompt("1-son")
+let p = prompt("2-son")
+let f = (q + p)
+
+// M - 11
+let s = alert("Misollar tugadi")
+
